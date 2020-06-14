2a5a7691efedabc1e84c9bbd6cae768597419045
Arun et <arunet008@gmail.com>
2020-06-14 23:08:37 +0530

    14/Jun

--- app.js
+++ app.js
@@ -5,21 +5,12 @@ var path = require('path');
 
 
 app.set('view engine', 'ejs')
-// var url = 'http://newsapi.org/v2/top-headlines?' +
-//           'country=us&' +
-//           'apiKey=a0e18f58e2fa406b8926f61b09fb6369';
-// var req = new Request(url);
-// fetch(req)
-//     .then(function(response) {
-//         console.log(response.json());
-//     })
-
 app.use(express.static(path.join(__dirname, '/public')));
 var options = {
 	method: 'GET',
     uri: 'http://newsapi.org/v2/top-headlines?country=us&',
     qs: {
-        apiKey  : 'a0e18f58e2fa406b8926f61b09fb6369' // -> uri + '?access_token=xxxxx%20xxxxx'
+        apiKey  : process.env.API_KEY // -> uri + '?access_token=xxxxx%20xxxxx'
     },
     headers: {
         'User-Agent': 'Request-Promise'
